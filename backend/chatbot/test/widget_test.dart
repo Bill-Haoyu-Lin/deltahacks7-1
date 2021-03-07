db29dcdc77884b66d27e8095d19c2818ebb14dcf
Bill <56321690+Bill-Haoyu-Lin@users.noreply.github.com>
2021-03-07 05:25:26 -0500

    update minor error

--- backend/chatbot/test/widget_test.dart
+++ backend/chatbot/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:chatbot_ui_final/main.dart';
+import 'package:chatbot/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
